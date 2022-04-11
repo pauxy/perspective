c82b5b0f6031890b90b0e38fe9a86bc7ea5a88b4
Pauxy <chunyu1399@outlook.com>
2022-04-11 23:28:10 +0800

    feat(gen2d): generate 2d array of 0 based on given height and width
    Signed-off-by: Pauxy <chunyu1399@outlook.com>

--- src/perspective.py
+++ src/perspective.py
@@ -1,3 +1,10 @@
 def gen2d(width,height):
-    arr = [] 
-    for i in range():
+    arr = []
+    if width<0 or height<0:
+        return arr
+    for i in range(height):
+        row = []
+        for i in range(width):
+            row.append(0)
+        arr.append(row)
+    return arr
--- tests/test_perspective.py
+++ tests/test_perspective.py
@@ -1,8 +1,8 @@
-import perspective
+import perspective as p
 import pytest
 
-def test_always_passes():
-    assert True
 
 def test_gen2d():
-    assert gen2d(1,1)==[0]
+    assert p.gen2d(1,1)==[[0]]
+    assert p.gen2d(-1,-1)==[]
+    assert p.gen2d(2,3)==[[0,0],[0,0],[0,0]]
